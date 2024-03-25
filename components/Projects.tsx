573fafc66428b51900ab6c323bb2a80f81e4afe6
EthanB87 <ethan@brockmanfamily.ca>
2024-03-25 10:47:04 -0400

    Image url 2

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,7 +12,7 @@ const Projects = () => {
         <ProjectCard title="Space Race" description="A 2d skill based platformer created in C# with Monogame/XNA" 
         techIcons={["/img/c-sharp.svg", "/img/MonoGame.svg"]} image="/img/space-race.png" githubLink="https://github.com/EthanB87/Space-Race"/>
         <ProjectCard title="NFL Statistical Analysis" description="Used Matplotlib and Python to visualize the top NFL offenses in rush & pass epa combined" 
-        techIcons={["/img/python.svg", "/img/matplotlib.svg"]} image="/img/rush_pass_epa.png" githubLink="https://github.com/EthanB87/NFL-Matplotlib"/>
+        techIcons={["/img/python.svg", "/img/matplotlib.svg"]} image="/img/rushpassepa.png" githubLink="https://github.com/EthanB87/NFL-Matplotlib"/>
         <ProjectCard title="Excel to SQL Converter" description="Created a WinForms application to add and display Excel files and sheets and then import the data to your SQL server" 
         techIcons={["/img/VB.Net.svg", "/img/VisualStudio.svg"]} image="/img/excel-sql.png" githubLink="https://github.com/EthanB87/Excel_to_SQL"/>
         <ProjectCard title="Movie Database App" description="Created a full stack application using .Net Core and Entity Framework for the API and ASP.Net Core for the front end" 
